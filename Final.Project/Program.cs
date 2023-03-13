b855c184805909b29278a0cb5d1005318747fdb4
jrmyreyes <rj.reyes024@gmail.com>
2023-03-12 22:09:02 -0700

    1.3

--- Final.Project/Program.cs
+++ Final.Project/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Transactions;
+﻿using System.Diagnostics.Metrics;
+using System.Transactions;
+using System.Xml.Linq;
 /*
  * Author: Ron Jeremy Reyes
  * Course: COMP-003A
@@ -11,54 +13,121 @@ namespace Final.Project
     {
         static void Main(string[] args)
         {
-            getInfo();                       
-        } 
+            GetInfo();
+        }
         // end main
 
         // methods start
         /// <summary>
         /// Asks for user's Name
         /// </summary>
-        static void getInfo()
+        static void GetInfo() // TODO: by convention, methods should be in PascalCase |>-----------------------------------------------------------[DONE]
         {
             PrintSeparator("Personal Information");
             // Get Name
             Console.Write("Enter First Name: "); // get first name
             string firstName = Console.ReadLine();
-           
-            while (string.IsNullOrEmpty(firstName) || isInt(firstName))
-            {
-                Console.WriteLine("\nInput cannot contain null/empty/numbers/special characters!");
-                Console.WriteLine("Please Try Again..." + "\n".PadRight(60, '_'));
-                ClearLine();
-                Console.Write("\nEnter First Name: ");
-                firstName = Console.ReadLine();
-            }
+            // TODO: consider creating a separate method for validating the firstName and lastName inputs as they perform the same logic |>--------[DONE]
+            InputName(firstName, "First");
 
             Console.Write("Enter Last Name: "); //get last name
             string lastName = Console.ReadLine();
+            // TODO: consider creating a separate method for validating the firstName and lastName inputs as they perform the same logic |>--------[DONE]
+            InputName(lastName, "Last");
 
-            while (string.IsNullOrEmpty(lastName) || isInt(lastName))
+            // Get Gender
+            // TODO: consider creating a separate method for this section. |>----------------------------------------------------------------------[DONE]
+            GetGender();
+
+            // Get Age
+            // TODO: create a separate method for this Age section
+            GetAge();
+
+            // convert name to UPPER CASE
+            string fName = firstName;
+            fName = fName.ToUpper();
+            string lName = lastName;
+            lName = lName.ToUpper();
+
+            Console.Clear();
+            PrintSeparator("Dating Form");
+            Console.WriteLine($"\nHello, {fName} {lName}!" + " \nPlease answer the following questions correctly and truthfully.\n"); // display name
+
+            PrintSeparator("Questions");
+            Questions();
+            Console.WriteLine($"\nHello, {fName} {lName}!" + " This are the things that summarizes what kind of person you are.");
+
+            Console.Write("\nThank you for answering!" + " Would you like to try again? Press [y] if yes or any key if not: ");
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                Console.Clear();
+                GetInfo();
+            }
+            else
+            {
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Section Separator
+        /// </summary>
+        /// <param name="header">Prints Section Header Name</param>
+        static void PrintSeparator(string header)
+        {
+            Console.WriteLine("".PadRight(50, '*') + $"\n\t{header} - Section\n" + "".PadRight(50, '*'));
+        }
+
+        /// <summary>
+        /// Name Input
+        /// </summary>
+        /// <param name="name">user input name</param>
+        /// <param name="_name">name parameter </param>
+        static void InputName(string name, string _name)
+        {
+            while (string.IsNullOrEmpty(name) || IsInt(name))
             {
                 Console.WriteLine("\nInput cannot contain null/empty/numbers/special characters!");
                 Console.WriteLine("Please Try Again..." + "\n".PadRight(60, '_'));
 
-                Console.Write("\nEnter Last Name: ");
-                lastName = Console.ReadLine();
+                Console.Write($"\nEnter {_name} Name: ");
+                name = Console.ReadLine();
             }
+        }
 
-            // Get Gender
+        /// <summary>
+        /// For Checking integer
+        /// </summary>
+        /// <param name="check">checks each char in string input if it has an integer value</param>
+        /// <returns></returns>  
+        static bool IsInt(string check) // TODO: by convention, methods should be in PascalCase
+        {
+            foreach (char c in check)
+            {
+                if (!char.IsLetter(c)) // https://learn.microsoft.com/en-us/dotnet/api/system.char.isletter?view=net-7.0
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Identifies user gender
+        /// </summary>
+        static void GetGender()
+        {
             PrintSeparator("Gender");
             try
             {
                 Console.Write("Enter your Gender | [M]-male | [F]-female | [O]-others |: ");
                 char getGender = Convert.ToChar(Console.ReadLine());
-                
+
                 if (getGender == 'M' || getGender == 'm')
                 {
                     Console.WriteLine("Male");
                 }
-                else if  (getGender == 'F' || getGender == 'f')
+                else if (getGender == 'F' || getGender == 'f')
                 {
                     Console.WriteLine("Female");
                 }
@@ -68,110 +137,44 @@ namespace Final.Project
                 }
                 else
                 {
-                    Console.WriteLine("\nTry Again" + "\nPress any key to continue...");
-                    Console.Read();
+                    Console.WriteLine("\nIvalid input!\nPlease Enter provided characters: [M/F/O]\n\nTry Again" + "\nPress any key to continue...");
+                    Console.ReadKey();
                     Console.Clear();
-                    getInfo();
+                    GetGender(); // TODO: an invalid input restarts this ENTIRE method. consider creating a separate method for the gender section, so it only repeats this section (not the entire getInfo()) [DONE]. you may also change your validation technique [PENDING]
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("\n\nError: Invalid Input\n" + ex.Message); // TODO: this Gender section should repeat if a string longer than 1 character is entered [DONE]
+                Console.ReadKey();
+                Console.Clear();
+                GetGender();
             }
+        }
 
-            // Get Age
+        static void GetAge()
+        {
             try
             {
                 PrintSeparator("Age");
-
                 Console.Write("Enter YEAR of Birth: ");
                 int year = Convert.ToInt32(Console.ReadLine());
 
                 AgeCalculator(year);
                 if (AgeCalculator(year) >= 18 && AgeCalculator(year) <= 120) // check age
                 {
-                    Console.Write($"Are you {AgeCalculator(year)} years old? [y] [n]:");
+                    Console.Write($"Is this your age [{AgeCalculator(year)}] years old? Press [y] if yes or any key if not: ");
                     if (Console.ReadKey().Key == ConsoleKey.Y)
                     {
-                        // convert name to UPPER CASE
-                        string fName = firstName;
-                        fName = fName.ToUpper();
-                        string lName = lastName;
-                        lName = lName.ToUpper();
-
-                        Console.Clear();
-                        PrintSeparator("Dating Form");
-                        Console.WriteLine($"\nHello, {fName} {lName}!" + " \nPlease answer the following questions correctly and truthfully.\n"); // display name
-
                         /* Questions Section */
                         PrintSeparator("Describe your interest/yourself");
-                        try
-                        {
-                            List<string> answers = new List<string>(); // |>--------------------------------------------------------------------- data collection
-
-                            Console.Write("\n1. What do you think is your favorite color: \n\t- ");
-                            answers.Add(Console.ReadLine());
-
-                            Console.Write("\n2. What is your favorite movie/series of all time: \n\t- ");
-                            answers.Add(Console.ReadLine());
-
-                            Console.Write("\n3. What is your favorite food: \n\t- ");
-                            answers.Add(Console.ReadLine());
-
-                            Console.Write("\n4. What is your favorite hobby: \n\t- ");
-                            answers.Add(Console.ReadLine());
-
-                            Console.Write("\n5. What is your spirit animal: \n\t- ");
-                            answers.Add(Console.ReadLine());
-
-                            Console.Write("\n6. What is the name of your favorite artist/band you listen to: \n\t- ");
-                            answers.Add(Console.ReadLine());
-
-                            Console.Write("\n7. What's something you're really passionate about: \n\t- ");
-                            answers.Add(Console.ReadLine());
-
-                            Console.Write("\n8. If you could travel anywhere in the world or outside of it, where would you go: \n\t- ");
-                            answers.Add(Console.ReadLine());
-
-                            Console.Write("\n9. What do you think is your love language: \n\t- ");
-                            answers.Add(Console.ReadLine());
-
-                            Console.Write("\n10. Describe yourself in one word: \n\t- ");
-                            answers.Add(Console.ReadLine());
-
-                            Console.Clear(); // clear console
-
-                            Console.WriteLine($"\nThis are the things that summarizes what kind of person you are {fName} {lName} [{AgeCalculator(year)}]\n" + "".PadRight(80, '_'));
-                            TraverseListAnswers(answers);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.Message);
-                            Console.ReadKey();
-                        }
-
-                        Console.Write("\nThank you for answering!" + " Would you like to try again? [y] [n]: ");
-                        if (Console.ReadKey().Key == ConsoleKey.Y)
-                        {
-                            Console.Clear();
-                            getInfo();
-                        }
-                        else if (Console.ReadKey().Key == ConsoleKey.N)
-                        {
-                            Console.WriteLine("\nPress any key to close the program...");
-                            Console.ReadKey();
-                        }
-                        else
-                        {
-                            Console.WriteLine("\nInvalid Input");
-                        }
                     }
                     else
                     {
-                        Console.WriteLine("\nError Input!\n\nPlease try again...");
+                        Console.WriteLine("\n\nPlease try again..."); // TODO: be more descriptive as to why there is an error |>------------------[REVISED]
                         Console.ReadKey();
                         Console.Clear();
-                        getInfo();
+                        GetAge(); // TODO: again, the entire form resets, instead of repeating just the this (Age) section. |>---------------------[DONE]
                     }
                 }
                 else if (AgeCalculator(year) < 18 && AgeCalculator(year) >= 0)
@@ -187,7 +190,7 @@ namespace Final.Project
                     Console.WriteLine("\nInvalid Input! Please try again...");
                     Console.ReadKey();
                     Console.Clear();
-                    getInfo();
+                    GetAge(); // TODO: again, the entire form resets, instead of repeating just the this (Age) section. |>---------------------[DONE]
                 }
             }
             catch (Exception ex)
@@ -195,41 +198,71 @@ namespace Final.Project
                 Console.WriteLine("Error: " + ex.Message + "\n\nTry Again...");
                 Console.ReadLine();
                 Console.Clear();
-                getInfo();
-            }        
-        }
-
-        /// <summary>
-        /// Section Separator
-        /// </summary>
-        /// <param name="header">Prints Section Header Name</param>
-        static void PrintSeparator(string header)
-        {
-            Console.WriteLine("".PadRight(50, '*') + $"\n\t{header} - Section\n" + "".PadRight(50, '*'));
-        }
-
-        public static void ClearLine()
-        {
-            Console.SetCursorPosition(0, Console.CursorTop - 3);
-            Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, Console.CursorTop - 3);
-        }
-
-        /// <summary>
-        /// For Checking integer
-        /// </summary>
-        /// <param name="check">checks each char in string input if it has an integer value</param>
-        /// <returns></returns>  
-        static bool isInt(string check)
-        {
-            foreach(char c in check)
-            {
-                if (!char.IsLetter(c)) // https://learn.microsoft.com/en-us/dotnet/api/system.char.isletter?view=net-7.0
-                {
-                    return true;
-                }               
+                GetAge(); // TODO: again, the entire form resets, instead of repeating just the this (Age) section.
             }
-            return false;
+        }
+
+        /// <summary>
+        /// Question Lists
+        /// </summary>
+        static void Questions()
+        {
+            // TODO: put your questions in a data structure as well |>-----------------------------------------------------------------------------[DONE]
+            // TODO: validate user responses to make sure it is not null or empty. consider creating a method for this purpose. |>-----------------[PENDING]
+            try
+            {
+                List<string> questions = new List<string>();
+                questions.Add("1. What do you think is your favorite color: ");
+                questions.Add("2.What is your favorite movie / series of all time: ");
+                questions.Add("3. What is your favorite food: ");
+                questions.Add("4. What is your favorite hobby: ");
+                questions.Add("5. What is your spirit animal: ");
+                questions.Add("6. What is the name of your favorite artist/band you listen to: ");
+                questions.Add("7. What's something you're really passionate about: ");
+                questions.Add("8. If you could travel anywhere in the world or outside of it, where would you go: ");
+                questions.Add("9. What do you think is your love language: ");
+                questions.Add("10. Describe yourself in one word: ");
+
+                List<string> answers = new List<string>(); // |>--------------------------------------------------------------------- data collection
+
+                Console.Write($"{questions[0]}\n\t- ");
+                answers.Add(Console.ReadLine());
+                Console.Write($"{questions[1]}\n\t- ");
+                answers.Add(Console.ReadLine());
+                Console.Write($"{questions[2]}\n\t- ");
+                answers.Add(Console.ReadLine());
+                Console.Write($"{questions[3]}\n\t- ");
+                answers.Add(Console.ReadLine());
+                Console.Write($"{questions[4]}\n\t- ");
+                answers.Add(Console.ReadLine());
+                Console.Write($"{questions[5]}\n\t- ");
+                answers.Add(Console.ReadLine());
+                Console.Write($"{questions[6]}\n\t- ");
+                answers.Add(Console.ReadLine());
+                Console.Write($"{questions[7]}\n\t- ");
+                answers.Add(Console.ReadLine());
+                Console.Write($"{questions[8]}\n\t- ");
+                answers.Add(Console.ReadLine());
+                Console.Write($"{questions[9]}\n\t- ");
+                answers.Add(Console.ReadLine());
+
+                Console.Clear(); // clear console                
+
+                Console.WriteLine($"".PadRight(50, '*') + "\tSummary" + "".PadRight(50, '*'));
+                int i = 0;
+                do
+                {
+                    Console.WriteLine($"{questions[i]} {answers[i]} ");
+                    i++;
+                } while (i < 10);
+
+                // TODO: this section should display the Question followed by the Response. (e.g., Question 1... Response 1... Question 2... Response 2...) [DONE]
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
         }
 
         /// <summary>
@@ -238,21 +271,8 @@ namespace Final.Project
         /// <param name="year">Calculates age based on year input</param>
         /// <returns>Age</returns>
         static int AgeCalculator(int year)
-        {           
-            return (DateTime.Now.Year - year);
-        }
-
-        /// <summary>
-        /// Traverse Answers
-        /// </summary>
-        /// <param name="answers">Prints answers</param>
-        static void TraverseListAnswers(List<string> answers)
         {
-            foreach (string answer in answers)
-            {
-                Console.WriteLine(answer);
-            }
-            Console.WriteLine("".PadRight(80, '_'));
+            return (DateTime.Now.Year - year);
         }
     }
 }
